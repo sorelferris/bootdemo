ab5db79adec39232da2208ceb7eba135137f2559
Sorel <925041482@qq.com>
2020-03-06 13:21:05 +0800

    this is the firs commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Sorel
+ * @version 1.0
+ * @date 2020/3/5
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
